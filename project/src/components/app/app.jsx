be28121a5b414073302de4ee0905c203960d7ec9
Marina Kotelnikov <oce4ka@gmail.com>
2021-06-02 04:08:38 +0300

    fixing linter errors

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Homepage from '../homepage/homepage';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {cityPlaceArray} = props;
+
+  App.propTypes = {
+    cityPlaceArray: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+      }),
+    ),
+  };
+
+  return <Homepage cityPlaceArray={cityPlaceArray}/>;
 }
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  CITY_PLACES: [0, 1, 2, 3, 4],
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App cityPlaceArray={Setting.CITY_PLACES}/>
   </React.StrictMode>,
   document.getElementById('root'));
