280ca8b6e58599baa9c0d0e91e95ad9fe7a73fda
Marina Kotelnikov <oce4ka@gmail.com>
2021-07-20 19:26:56 +0300

    smooth scrolling

--- project/src/components/review-list/review-list.jsx
+++ project/src/components/review-list/review-list.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import ReviewItem from '../review-item/review-item';
 import PropTypes from 'prop-types';
 import reviewProp from '../review-item/review.prop';
@@ -8,6 +8,7 @@ import {Settings} from '../../const';
 function ReviewList(props) {
   const {reviews} = props;
   const reviewsCount = reviews.length;
+
   return (
     !!reviewsCount && // !! - to avoid "0"
     <section className="property__reviews reviews">
